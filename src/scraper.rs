cf69cfc0ab7bf0028e3eb123ea7b61ada6de4dbc
Mariusz Bialonczyk <manio@skyboo.net>
2023-04-07 14:54:34 +0200

    Revert "Merge branch 'new-api'"
    This reverts commit 5a18ddfa914c44f34fdc040f897e086c6f938f95, reversing
    changes made to 90bb889c1b973736258add7d59cff4d2af1ecacd.

--- src/scraper.rs
+++ src/scraper.rs
@@ -3,7 +3,7 @@ use std::collections::HashMap;
 use std::time::Instant;
 
 static URL: &str = "https://logowanie.tauron-dystrybucja.pl/login";
-static ENERGY_URL: &str = "https://elicznik.tauron-dystrybucja.pl/energia/api";
+static CHART_URL: &str = "https://elicznik.tauron-dystrybucja.pl/index/charts";
 static SERVICE: &str = "https://elicznik.tauron-dystrybucja.pl";
 
 pub struct Scraper {
@@ -11,11 +11,11 @@ pub struct Scraper {
     pub username: String,
     pub password: String,
     pub start_date: String,
-    pub end_date: String,
+    pub end_date: Option<String>,
 }
 
 impl Scraper {
-    pub async fn get_json_data(&self) -> Result<(String, String), Box<dyn std::error::Error>> {
+    pub async fn get_json_data(&self) -> Result<String, Box<dyn std::error::Error>> {
         //login parameters
         let mut payload = HashMap::new();
         payload.insert("username", &self.username);
@@ -24,10 +24,14 @@ impl Scraper {
         payload.insert("service", &service);
 
         //chart data parameters
-        let mut params: HashMap<&str, &str> = HashMap::new();
-        params.insert("from", &self.start_date);
-        params.insert("to", &self.end_date);
-        params.insert("profile", "full time");
+        let mut params = HashMap::new();
+        params.insert("dane[paramType]", "csv");
+        params.insert("dane[trybCSV]", "godzin");
+        params.insert("dane[startDay]", &self.start_date);
+        if let Some(date) = &self.end_date {
+            params.insert("dane[endDay]", &date);
+        }
+        params.insert("dane[checkOZE]", "on");
 
         //creating client with cookie store
         let client = reqwest::Client::builder().cookie_store(true).build()?;
@@ -55,30 +59,19 @@ impl Scraper {
             self.name, ms
         );
 
-        let mut imported = String::new();
-        let mut exported = String::new();
-        for type_ in &["consum", "oze"] {
-            info!(
-            "{}: Requesting `{}` JSON data, start date: <b><cyan>{}</>, end date: <b><cyan>{:?}</>",
-            self.name, type_, self.start_date, self.end_date
+        info!(
+            "{}: Requesting JSON data, start date: <b><cyan>{}</>, end date: <b><cyan>{:?}</>",
+            self.name, self.start_date, self.end_date
         );
-            sub_started = Instant::now();
-            let mut params = params.clone();
-            params.insert("type", type_);
-            let res = client.post(ENERGY_URL).form(&params).send().await?;
-            let t = res.text().await?;
-            match type_ {
-                &"consum" => imported = t,
-                &"oze" => exported = t,
-                _ => (),
-            };
-            let elapsed = sub_started.elapsed();
-            let ms = (elapsed.as_secs() * 1_000) + (elapsed.subsec_nanos() / 1_000_000) as u64;
-            info!(
-                "{}: <black>- - -</> ⏱️ response time: <magenta>{}</> ms",
-                self.name, ms
-            );
-        }
+        sub_started = Instant::now();
+        let res = client.post(CHART_URL).form(&params).send().await?;
+        let t = res.text().await?;
+        let elapsed = sub_started.elapsed();
+        let ms = (elapsed.as_secs() * 1_000) + (elapsed.subsec_nanos() / 1_000_000) as u64;
+        info!(
+            "{}: <black>- - -</> ⏱️ response time: <magenta>{}</> ms",
+            self.name, ms
+        );
 
         let elapsed = started.elapsed();
         let ms = (elapsed.as_secs() * 1_000) + (elapsed.subsec_nanos() / 1_000_000) as u64;
@@ -86,6 +79,6 @@ impl Scraper {
             "{}: ⌛ total scraping time: <magenta>{}</> ms",
             self.name, ms
         );
-        Ok((imported, exported))
+        Ok(t)
     }
 }
