24dc20f61e8288dc4fa9e1a85cf7728c1266835a
Mariusz Bialonczyk <manio@skyboo.net>
2023-04-11 08:49:47 +0200

    parser: csv parse/deserialize

--- src/parser.rs
+++ src/parser.rs
@@ -1,36 +1,48 @@
+use chrono::Timelike;
 use serde::de;
 use serde::Deserialize;
 use serde::Deserializer;
 use serde_json::Value;
 use simplelog::*;
 use std::io::BufRead;
+use std::ops::{Add, Sub};
 use std::str::FromStr;
 
 #[derive(Deserialize, Debug)]
-#[allow(non_snake_case)]
 pub struct Entry {
-    #[serde(deserialize_with = "de_type_from_str")]
-    pub EC: f64,
-    pub Date: chrono::NaiveDate,
-    #[serde(deserialize_with = "de_type_from_str")]
-    pub Hour: u8,
-    #[serde(deserialize_with = "de_type_from_str")]
-    pub Status: u8,
-    #[serde(deserialize_with = "de_bool_from_str")]
-    pub Extra: bool,
-    #[serde(deserialize_with = "de_type_from_str")]
-    pub Zone: u8,
-    pub ZoneName: String,
-    pub Taryfa: Option<String>,
+    #[serde(rename = "Data", deserialize_with = "de_datetime_from_str")]
+    pub date_time: chrono::NaiveDateTime,
+    #[serde(rename = " Wartość kWh", deserialize_with = "de_float_from_str")]
+    pub kwh_value: f64,
+    #[serde(rename = "Rodzaj", deserialize_with = "de_bool_from_str")]
+    pub imported: bool,
 }
 
-fn de_type_from_str<'de, T: FromStr, D>(deserializer: D) -> Result<T, D::Error>
+fn de_datetime_from_str<'de, D>(deserializer: D) -> Result<chrono::NaiveDateTime, D::Error>
 where
     D: Deserializer<'de>,
-    <T as FromStr>::Err: std::fmt::Display,
+{
+    let s = String::deserialize(deserializer)?;
+    //Tauron is formatting the hours from 1:00 to 24:00,
+    //thus making it `out of range` for automatic parsing
+    //Read with a "trick", which is: reading hour as a minute
+    //and make a correction afterwards
+    chrono::NaiveDateTime::parse_from_str(&s, "%Y-%m-%d %M:%H")
+        .map_err(serde::de::Error::custom)
+        .map(|i| {
+            i.add(chrono::Duration::hours((i.minute() - 1).into()))
+                .sub(chrono::Duration::minutes((i.minute()).into()))
+        })
+}
+
+fn de_float_from_str<'de, D>(deserializer: D) -> Result<f64, D::Error>
+where
+    D: Deserializer<'de>,
 {
     let s = <String>::deserialize(deserializer)?;
-    T::from_str(&s).map_err(de::Error::custom)
+    //Tauron is formatting values like: "0,774" instead of "0.774"
+    //we need to change comma to dot before float parsing
+    s.replace(',', ".").parse().map_err(de::Error::custom)
 }
 
 fn de_bool_from_str<'de, D>(deserializer: D) -> Result<bool, D::Error>
@@ -39,9 +51,9 @@ where
 {
     let s = <String>::deserialize(deserializer)?;
     match s.as_ref() {
-        "N" => Ok(false),
-        "T" => Ok(true),
-        _ => Err(de::Error::custom("Field is not T/N")),
+        "pobór" => Ok(true),
+        "oddanie" => Ok(false),
+        _ => Err(de::Error::custom("Field is not `pobór`/`oddanie`")),
     }
 }
 
